94c72b76bbd8d19d88ce339adaa00d911ecc573d
edgy-raven <65307295+edgy-raven@users.noreply.github.com>
2023-01-13 13:59:34 -0500

    add mmr

--- bridge_discord/datastore/profile.py
+++ bridge_discord/datastore/profile.py
@@ -1,5 +1,10 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+import functools
+
+import numpy as np
+import scipy
+from sqlalchemy import Column, ForeignKey, Float, Integer, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext import hybrid_property
 
 from .basic import Base
 
@@ -19,9 +24,32 @@ class ServerProfile(Base):
 class BBOProfile(Base):
     __tablename__ = "bbo_profile"
     bbo_user = Column(String, primary_key=True)
+    mmr_m = Column(Float, server_default=1200.0)
+    mmr_s = Column(Float, server_default=400.0)
 
     discord_main = relationship("BBOMain", uselist=False, backref="bbo_profile")
     discord_represented = relationship("BBORepresentative", backref="bbo_profile")
+
+    def update_mmr(self, other, win):
+        # Message passing with approximate moment matching, aka TrueSkill
+        def integrand(r, moment):
+            other_cdf = scipy.stats.norm.cdf(r, other.mmr_m, other.mmr_s)
+            return r ** moment * (
+                scipy.stats.norm.pdf(r, self.mmr_m, self.mmr_s) *
+                other_cdf if win else (1.0 - other_cdf) /
+                scipy.stats.norm.cdf(
+                    0,
+                    other.mmr_m - self.mmr_m if win else self.mmr_m - other.mmr_m,
+                    (self.mmr_s * self.mmr_s + other.mmr_s) ** 0.5
+                )
+            )
+        self.mmr_m = scipy.integrate.quad(functools.partial(integrand, moment=1), -np.inf, np.inf)[0]
+        m2 = scipy.integrate.quad(functools.partial(integrand, moment=2), -np.inf, np.inf)[0]
+        self.mmr_s = (m2 - self.mmr_m*self.mmr_m)**0.5
+
+    @hybrid_property
+    def conservative_estimate(self):
+        return self.mmr_m - 3.0 * self.mmr_s
 
 
 class BBOMain(Base):
